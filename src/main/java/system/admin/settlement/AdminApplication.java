3f2960444ed4335e11264351b6f33fa72043de94
Moon <mjskks94@naver.com>
2022-07-31 19:50:05 +0900

    initial commits

---
+++ src/main/java/system/admin/settlement/AdminApplication.java
@@ -0,0 +1,14 @@
+package system.admin.settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
