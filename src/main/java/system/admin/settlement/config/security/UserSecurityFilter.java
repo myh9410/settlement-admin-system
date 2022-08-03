76af5d65926fa717ffb62cb967a00e907d979fcd
myh9410 <mjskks94@naver.com>
2022-08-03 20:04:04 +0900

    security filter 개발 - security config, filter 추가

--- src/main/java/system/admin/settlement/config/security/UserSecurityFilter.java
+++ src/main/java/system/admin/settlement/config/security/UserSecurityFilter.java
@@ -1,2 +1,30 @@
-package system.admin.settlement.config.security;public class UserSecurityFilter {
+package system.admin.settlement.config.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class UserSecurityFilter extends OncePerRequestFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(UserSecurityFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            log.info("authorize check");
+        } catch (Exception e) {
+            log.error("Invalid Token :: error - {}", e.getMessage());
+        } finally {
+            filterChain.doFilter(request, response);
+        }
+    }
+
 }
