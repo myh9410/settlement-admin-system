1ce7e111001bb5e46cd2797745abaf2e8f6bab65
myh9410 <mjskks94@naver.com>
2022-08-03 20:04:04 +0900

    security filter 개발 - security config, filter 추가

--- src/main/java/system/admin/settlement/config/SecurityConfig.java
+++ src/main/java/system/admin/settlement/config/SecurityConfig.java
@@ -1,33 +1,55 @@
 package system.admin.settlement.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import system.admin.settlement.config.security.UserSecurityFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
     //WebSecurityConfigurerAdapter deprecated
+
+    private final UserSecurityFilter userSecurityFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
                 .httpBasic().disable()
-                .cors().and()
+                .cors()
 
-//                .addFilterBefore(??,UsernamePasswordAuthenticationFilter.class)
-//                .authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
-//                .anyRequest().authenticated().and()
+                .and()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+
+                .addFilterBefore(userSecurityFilter,UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests().antMatchers("/admin/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+
+                .and()
+
                 .build();
     }
 
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().antMatchers("");
-//    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/resources/**");
+    }
 }
