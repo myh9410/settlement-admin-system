a8eee5bea3f6f794c26d17049ca7e8ad1f5e6f51
Moon <mjskks94@naver.com>
2022-08-05 22:43:35 +0900

    controller 개발 - UserController 추가

---
+++ src/main/java/system/admin/settlement/controllers/UserController.java
@@ -0,0 +1,11 @@
+package system.admin.settlement.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    //todo : 회원가입, 로그인
+}
