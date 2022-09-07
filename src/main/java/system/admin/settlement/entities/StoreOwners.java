684b6c75d57eed564a101ae28c211b1db65f0fa6
myh9410 <mjskks94@naver.com>
2022-09-07 14:34:53 +0900

    storeOwners 도메인 개발 - storeOwners entity 수정

--- src/main/java/system/admin/settlement/entities/StoreOwners.java
+++ src/main/java/system/admin/settlement/entities/StoreOwners.java
@@ -1,21 +1,31 @@
 package system.admin.settlement.entities;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
+
+/**
+ * CREATE TABLE `settlement`.`store_owners` (
+ *   `id` INT NOT NULL AUTO_INCREMENT,
+ *   `name` VARCHAR(45) NOT NULL,
+ *   PRIMARY KEY (`id`));
+ */
 
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
+@Table(name = "store_owners")
 public class StoreOwners {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String name;
 
     @OneToMany(mappedBy = "order")
