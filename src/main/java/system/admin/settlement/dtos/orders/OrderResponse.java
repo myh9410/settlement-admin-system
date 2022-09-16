686be1cbd5eda6b7dc72c5312d3cfe638235d096
myh9410 <mjskks94@naver.com>
2022-09-16 17:05:21 +0900

    Orders 도메인 개발 - resopnseDto에 엔티티 활용한 빌더 추가

--- src/main/java/system/admin/settlement/dtos/orders/OrderResponse.java
+++ src/main/java/system/admin/settlement/dtos/orders/OrderResponse.java
@@ -1,6 +1,7 @@
 package system.admin.settlement.dtos.orders;
 
 import lombok.*;
+import system.admin.settlement.entities.Orders;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +12,14 @@ import java.time.LocalDateTime;
 @Builder
 public class OrderResponse {
 
+    private Long id;
+
     private LocalDateTime createdDate;
+
+    @Builder(builderMethodName = "builderByOrders", builderClassName = "OrdersBuilder")
+    public OrderResponse(Orders orders) {
+        id = orders.getId();
+        createdDate = orders.getCreatedDate();
+    }
 
 }
